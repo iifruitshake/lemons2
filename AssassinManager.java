90b58bc90210b2231724a800b2a6b4331a2d583e
iifruitshake <92800647+iifruitshake@users.noreply.github.com>
2022-01-07 08:48:35 -0600

    Update AssassinManager.java

--- AssassinManager.java
+++ AssassinManager.java
@@ -1,5 +1,38 @@
 public class AssassinManager {
     // YOUR CODE GOES HERE
-
+	AssassinNode killring;
+	AssassinNode graveyard;
+	
+	public AssassinManager(List<String> names) {
+		
+	}
+	
+	public String killRing() {
+		
+	}
+	
+	public String graveyard() {
+		
+	}
+	
+	public boolean killRingContains(String name) {
+		
+	}
+	
+	public boolean graveyardContains(String name) {
+		
+	}
+	
+	public boolean isGameOver() {
+		
+	}
+	
+	public String winner() {
+		
+	}
+	
+	public void kill(String name) {
+		
+	}
 
 }
