07288ab1d986cf90ffc8331541fd743de7304337
iifruitshake <92800647+iifruitshake@users.noreply.github.com>
2022-01-10 09:15:21 -0600

    Update AssassinManager.java

--- AssassinManager.java
+++ AssassinManager.java
@@ -21,7 +21,10 @@ public class AssassinManager {
 		Node current = killring;
 		while(current!=null) {	
 			
-			f+=current.name+" ";
+			if(current.next!=null)
+				f+=current.name+" is stalking "+current.next.name+"\n";
+			else
+				f+=current.name+" is stalking "+killring.name;
 			
 			current= current.next;
 		}
@@ -33,7 +36,7 @@ public class AssassinManager {
 		Node current = graveyard;
 		while(current!=null) {	
 			
-			f+=current.name+" ";
+			f+=current.name+" was killed by "+current.killer+"\n";
 			
 			current= current.next;
 		}
@@ -43,10 +46,13 @@ public class AssassinManager {
 	public boolean killRingContains(String name) {
 		name.toLowerCase();
 		Node current = killring;
-		while(current!=null) {
+		while(current!=null)
+		{
 			System.out.println(name);
 			if(current.name.toLowerCase().equals(name))
+			{
 				return true;
+			}
 			current=current.next;
 		}
 		return false;
@@ -55,9 +61,13 @@ public class AssassinManager {
 	public boolean graveyardContains(String name) {
 		String f =name.toLowerCase();
 		Node current = graveyard;
-		while(current!=null) {
+		while(current!=null)
+		{
+			System.out.println(f);
 			if(current.name.toLowerCase().equals(f))
+			{
 				return true;
+			}
 			current=current.next;
 		}
 		return false;
@@ -84,7 +94,46 @@ public class AssassinManager {
 	}
 	
 	public void kill(String name) {
-		return;
+		name.toLowerCase();
+		Node current = killring;
+   		Node b1=killring;
+    	while(current !=null)
+    	{
+    		if(current.name.toLowerCase().equals(name))
+   			{
+    			current.killer=b1.name;
+    			
+    			if(graveyard==null)
+    				graveyard=new Node(current.name);
+    			else
+    				graveyard=new Node(current.name,graveyard);
+    			
+    			b1.next = current.next;
+   			}
+    		b1=current;
+    		current=current.next;
+    	}
+   		
+    	
+    	
 	}
 
+
+	
+	
+	public static void main(String[] args)
+	{
+		ArrayList<String> f= new ArrayList<String>();
+		f.add("Cumden");
+		f.add("Pen");
+		f.add("is");
+		f.add("sm");
+		f.add("big");
+		AssassinManager egg = new AssassinManager(f);
+		egg.kill("cumden");
+		System.out.println(egg.killRing());
+		
+		
+		
+	}
 }
